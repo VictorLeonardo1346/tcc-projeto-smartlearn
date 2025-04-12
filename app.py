c787742399051e23cfaf814cfba9fe2e1e03b0f8
VictorLeonardo1346 <victorleosilva18@gmail.com>
2025-04-12 11:20:50 -0300

    login, questionario e aleatorização de perguntas

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ import customtkinter as ctk
 from tkinter import *
 from PIL import Image, ImageTk
 import sqlite3
-
+import webbrowser
+import os
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+import random
 
 class BackEnd():
     def conecta_db(self):
-        self.conn = sqlite3.connect("Sistema_cadastros.db")
+        caminho_absoluto = os.path.abspath("Sistema_cadastros.db")
+        self.conn = sqlite3.connect(caminho_absoluto)
         self.cursor = self.conn.cursor()
-        print("Banco de dados criado com sucesso")
+        print("Banco de dados criado/conectado com sucesso em:", caminho_absoluto)
 
     def desconecta_db(self):
         self.conn.close()
@@ -17,15 +22,13 @@ class BackEnd():
 
     def cria_tabela(self):
         self.conecta_db()
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS Usuarios(
-                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Username TEXT NOT NULL,
-                Email TEXT NOT NULL,
-                Senha TEXT NOT NULL,
-                Confirma_Senha TEXT NOT NULL
-            );
-        """)
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS Usuarios(
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Username TEXT NOT NULL,
+            Email TEXT NOT NULL,
+            Senha TEXT NOT NULL,
+            Confirma_Senha TEXT NOT NULL
+        );""")
         self.conn.commit()
         print("Tabela criada com sucesso!")
         self.desconecta_db()
@@ -35,11 +38,6 @@ class BackEnd():
         self.email_cadastro = self.email_cadastro_entry.get()
         self.senha_cadastro = self.senha_cadastro_entry.get()
         self.confirma_senha_cadastro = self.confirma_senha_entry.get()
-
-        print("Usuário:", self.username_cadastro)
-        print("Email:", self.email_cadastro)
-        print("Senha:", self.senha_cadastro)
-        print("Confirmação:", self.confirma_senha_cadastro)
 
         self.conecta_db()
 
@@ -53,9 +51,7 @@ class BackEnd():
             elif(self.senha_cadastro != self.confirma_senha_cadastro):
                 messagebox.showwarning(title="Sistema de login", message="As senhas não coincidem.")
             else:
-                self.cursor.execute("""
-                    INSERT INTO Usuarios (Username, Email, Senha, Confirma_Senha)
-                    VALUES (?, ?, ?, ?)""", (self.username_cadastro, self.email_cadastro, self.senha_cadastro, self.confirma_senha_cadastro))
+                self.cursor.execute("""INSERT INTO Usuarios (Username, Email, Senha, Confirma_Senha) VALUES (?, ?, ?, ?)""", (self.username_cadastro, self.email_cadastro, self.senha_cadastro, self.confirma_senha_cadastro))
                 self.conn.commit()
                 messagebox.showinfo(title="Sistema de login", message=f"Parabéns {self.username_cadastro}, seus dados foram cadastrados com sucesso!")
                 self.limpa_entry_cadastro()
@@ -79,6 +75,7 @@ class BackEnd():
             elif self.verifica_dados:
                 messagebox.showinfo(title="Sistema de Login", message=f"Parabéns {self.username_login}, login feito com sucesso!")
                 self.limpa_entry_login()
+                self.pagina_inicial()
             else:
                 messagebox.showerror(title="Sistema de login", message="Dados não encontrados no sistema. Verifique suas informações ou cadastre-se.")
         except Exception as e:
@@ -124,9 +121,6 @@ class App(ctk.CTk, BackEnd):
         self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=330, placeholder_text="Sua senha..", font=("Century Gothic", 16), corner_radius=15, show="°")
         self.senha_login_entry.grid(row=2, column=0, pady=10, padx=10)
 
-        self.ver_senha = ctk.CTkCheckBox(self.frame_login, text="Clique para ver a senha", font=("Century Gothic", 12), corner_radius=20)
-        self.ver_senha.grid(row=3, column=0, pady=10, padx=10)
-
         self.btn_login = ctk.CTkButton(self.frame_login, width=330, text="FAZER LOGIN", font=("Century Gothic", 16), corner_radius=15, command=self.verifica_login)
         self.btn_login.grid(row=4, column=0, pady=10, padx=10)
 
@@ -137,32 +131,47 @@ class App(ctk.CTk, BackEnd):
         for widget in self.winfo_children():
             widget.destroy()
 
+        self.img = self.redimensionar_imagem("logi-img.png", 400, 600)
+        self.lb_img = ctk.CTkLabel(self, text=None, image=self.img)
+        self.lb_img.place(x=0, y=0)
+
         self.frame_cadastro = ctk.CTkFrame(self, width=350, height=580)
         self.frame_cadastro.place(x=420, y=10)
 
-        self.lb_title_cadastro = ctk.CTkLabel(self.frame_cadastro, text="Crie sua conta", font=("Century Gothic bold", 24))
-        self.lb_title_cadastro.grid(row=0, column=0, padx=10, pady=10)
+        self.lb_title = ctk.CTkLabel(self.frame_cadastro, text="Cadastro de Novo Usuário", font=("Century Gothic bold", 24))
+        self.lb_title.grid(row=0, column=0, padx=10, pady=10)
 
-        self.username_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=330, placeholder_text="Seu nome de usuário..", font=("Century Gothic", 16), corner_radius=15)
-        self.username_cadastro_entry.grid(row=1, column=0, pady=5, padx=10)
+        self.username_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=330, placeholder_text="Escolha um nome de usuário..", font=("Century Gothic", 16), corner_radius=15)
+        self.username_cadastro_entry.grid(row=1, column=0, pady=10, padx=10)
 
-        self.email_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=330, placeholder_text="Email..", font=("Century Gothic", 16), corner_radius=15)
-        self.email_cadastro_entry.grid(row=2, column=0, pady=5, padx=10)
+        self.email_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=330, placeholder_text="Digite seu e-mail..", font=("Century Gothic", 16), corner_radius=15)
+        self.email_cadastro_entry.grid(row=2, column=0, pady=10, padx=10)
 
-        self.senha_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=330, placeholder_text="Senha..", font=("Century Gothic", 16), corner_radius=15, show="°")
-        self.senha_cadastro_entry.grid(row=3, column=0, pady=5, padx=10)
+        self.senha_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=330, placeholder_text="Escolha uma senha..", font=("Century Gothic", 16), corner_radius=15, show="°")
+        self.senha_cadastro_entry.grid(row=3, column=0, pady=10, padx=10)
 
         self.confirma_senha_entry = ctk.CTkEntry(self.frame_cadastro, width=330, placeholder_text="Confirme sua senha..", font=("Century Gothic", 16), corner_radius=15, show="°")
-        self.confirma_senha_entry.grid(row=4, column=0, pady=5, padx=10)
+        self.confirma_senha_entry.grid(row=4, column=0, pady=10, padx=10)
 
-        self.ver_senha_cadastro = ctk.CTkCheckBox(self.frame_cadastro, text="Clique para ver a senha", font=("Century Gothic", 12), corner_radius=20)
-        self.ver_senha_cadastro.grid(row=5, column=0, pady=10, padx=5)
+        self.btn_cadastrar = ctk.CTkButton(self.frame_cadastro, width=330, text="FAZER CADASTRO", font=("Century Gothic", 16), corner_radius=15, command=self.cadastrar_user)
+        self.btn_cadastrar.grid(row=6, column=0, pady=10, padx=10)
 
-        self.btn_cadastrar_user = ctk.CTkButton(self.frame_cadastro, width=330, fg_color="green", hover_color="#050", text="CADASTRAR", font=("Century Gothic", 16), corner_radius=15, command=self.cadastrar_user)
-        self.btn_cadastrar_user.grid(row=6, column=0, pady=5, padx=10)
+    def pagina_inicial(self):
+        for widget in self.winfo_children():
+            widget.destroy()
 
-        self.btn_voltar_login = ctk.CTkButton(self.frame_cadastro, width=330, text="VOLTAR PARA LOGIN", font=("Century Gothic", 16), corner_radius=15, fg_color="#444", hover_color="#333", command=self.tela_de_login)
-        self.btn_voltar_login.grid(row=7, column=0, pady=5, padx=10)
+        self.lb_boas_vindas = ctk.CTkLabel(self, text=f"Bem-vindo(a), {self.username_login}!", font=("Century Gothic", 24))
+        self.lb_boas_vindas.pack(pady=40)
+
+        self.btn_questionario = ctk.CTkButton(self, text="IR PARA QUESTIONÁRIO", font=("Century Gothic", 16), corner_radius=15, command=self.abrir_questionario)
+        self.btn_questionario.pack(pady=10)
+
+        self.btn_logout = ctk.CTkButton(self, text="SAIR DO SISTEMA", font=("Century Gothic", 16), corner_radius=15, fg_color="red", hover_color="#800", command=self.tela_de_login)
+        self.btn_logout.pack(pady=10)
+
+    def abrir_questionario(self):
+        caminho_html = os.path.abspath("questionario/index.html")
+        webbrowser.open_new_tab(f"file:///{caminho_html}")
 
     def limpa_entry_cadastro(self):
         self.username_cadastro_entry.delete(0, END)
@@ -173,7 +182,6 @@ class App(ctk.CTk, BackEnd):
     def limpa_entry_login(self):
         self.username_login_entry.delete(0, END)
         self.senha_login_entry.delete(0, END)
-
 
 if __name__ == "__main__":
     app = App()
